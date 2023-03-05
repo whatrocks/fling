88261ae0b643873f0e28c9424a768b1506a07f9f
Charlie Harrington <charles.harrington@gmail.com>
2023-03-05 13:32:24 -0800

    more attempts at linting

--- index.ts
+++ index.ts
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander'
+import { Command } from 'commander';
 
-import { spinnerError, stopSpinner } from './spinner'
-import { widgets } from "./widgets";
+import { spinnerError, stopSpinner } from './spinner';
+import { widgets } from './widgets';
 
-const program  = new Command();
-program.option("-v, --verbose", "verbose logging");
-program.description('Fling - a not-so serious Farcaster client')
+const program = new Command();
+program.option('-v, --verbose', 'verbose logging');
+program.description('Fling - a not-so serious Farcaster client');
 program.version('0.0.1');
 program.addCommand(widgets);
 
-process.on("unhandledRejection", function(err: Error) {
-    const debug = program.opts().verbose;
-    if (debug) {
-        console.error(err.stack);
-    }
-    spinnerError();
-    stopSpinner();
-    program.error('', { exitCode: 1});
-})
+process.on('unhandledRejection', function (err: Error) {
+  const debug = program.opts().verbose;
+  if (debug) {
+    console.error(err.stack);
+  }
+  spinnerError();
+  stopSpinner();
+  program.error('', { exitCode: 1 });
+});
 
 async function main() {
-    await program.parseAsync();
+  await program.parseAsync();
 }
 
 console.log();
--- widgets.ts
+++ widgets.ts
@@ -1,38 +1,39 @@
-import { Command } from "commander";
-import { 
-    spinnerError,
-    spinnerInfo,
-    spinnerSuccess,
-    updateSpinnerText
-} from "./spinner";
+import { Command } from 'commander';
+import {
+  spinnerError,
+  spinnerInfo,
+  spinnerSuccess,
+  updateSpinnerText,
+} from './spinner';
 
-export const widgets = new Command("widgets");
+export const widgets = new Command('widgets');
 
-widgets.command("list").action(async () => {
-    updateSpinnerText("Processing ");
-    // do work
-    await new Promise(resolve => {
-        setTimeout(resolve, 1000);
-    });
+widgets.command('list').action(async () => {
+  updateSpinnerText('Processing ');
+  // do work
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+  });
+  spinnerSuccess();
+  console.table([
+    { id: 1, name: 'Tommy' },
+    { is: 2, name: 'Betty' },
+  ]);
+});
+
+widgets
+  .command('get')
+  .argument('<id>', 'the id of the widget')
+  .option('-f, --format <format>', 'the format of the widget')
+  .action(async (id, options) => {
+    updateSpinnerText('Getting widget ' + id);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     spinnerSuccess();
-    console.table([
-        { id: 1, name: "Tommy"},
-        { is: 2, name: "Betty"},
-    ])
-})
+    console.table({ id: 1, name: 'Tommy' });
+  });
 
-widgets.command("get")
-.argument("<id>", "the id of the widget")
-.option("-f, --format <format>","the format of the widget")
-.action(async (id, options) => {
-    updateSpinnerText("Getting widget " + id);
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    spinnerSuccess();
-    console.table({id: 1, name: "Tommy"});
-})
-
-widgets.command("unhandled-error").action(async () => {
-    updateSpinnerText("Processing an unhandled failure ");
-    await new Promise(resolve => setTimeout(resolve, 3000));
-    throw new Error("Unhandled error");
-})
+widgets.command('unhandled-error').action(async () => {
+  updateSpinnerText('Processing an unhandled failure ');
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error('Unhandled error');
+});
